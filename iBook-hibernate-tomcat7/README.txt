d26c6bc4bbf7d9d4c9eb8a0624227910c885a9a3
anna.manukyan <anna.mnkyn@gmail.com>
2012-07-09 10:28:30 +0200

    The readme file is updated. The sql file is changed.

--- iBook-hibernate-tomcat7/README.txt
+++ iBook-hibernate-tomcat7/README.txt
@@ -1,3 +1,6 @@
+How to run the example
+======================
+
 The project demonstrates how to enable clustered Infinispan 2nd level cache for Hibernate under Tomcat7.
 
 The project is a simple iBook - store of books, which allows user to view books, add them to their wishlist and then checkout.
@@ -5,16 +8,58 @@ It uses MySQL database.
 
 The project configuration is done for working under Tomcat server.
 
+
+Configuring Tomcat7 for clustering
+------------------------------------
+
 The configuration and clustering enable should be done with this steps:
 
-1. Enable tomcat clustering using guide http://tomcat.apache.org/tomcat-7.0-doc/cluster-howto.html (see the clustering.xml in the project).
+1. Enable tomcat clustering using guide http://tomcat.apache.org/tomcat-7.0-doc/cluster-howto.html (see the server.xml in the project).
 Change the Membership address to 224.0.0.0 .
 
 2. Make sure that the multicast is enabled on your machine, and add new multicast route using this command:
 
-route add -net 224.0.0.0 netmask 240.0.0.0 dev lo
+   `route add -net 224.0.0.0 netmask 240.0.0.0 dev lo`
 
 3. For being able to have replicated 2nd level cache, the JGroups transport should be enabled. For some JDK impementations
 it doesn't work properly, so I had to add the preferIPv4Stack property to JAVA_OPTS.
+   `JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true"`
+
+4. If you want to test application on 2 instances of Tomcat running on the same machine, modify the ports in server.xml
+of two different instances of tomcat accordingly.
+
+5. Create MySQL datasource in Tomcat context.xml (see $PROJECT_HOME/context.xml):
+
+    ` <Resource name="jdbc/iBook" auth="Container" type="javax.sql.DataSource"
+                   maxActive="100" maxIdle="30" maxWait="10000"
+                   username="root" password="" driverClassName="com.mysql.jdbc.Driver"
+                   url="jdbc:mysql://localhost:3306/iBook"/>`
+
+6. Add MySQL JDBC driver jar to tomcat libraries.
+
+7. Prepare your MySQL DB:
+
+       `iBook` (run iBook.sql for structure and data)
+
+      accessible with the following credentials:
+
+       `username: root`
+
+
+Deploying application
+------------------------------------
+
+
+1. Change the value of the following property in pom.xml to your Tomcat path:
+
+   `<deploy.path>/usr/share/apache-tomcat-7.0.27/webapps/</deploy.path>`
+
+2. For building and running app using, build and deploy the application with command:
+
+    `mvn clean package install`
+
+3. Start your Tomcat server.
+
+4. Go to http://localhost:8080/iBook
 
 
--- iBook-hibernate-tomcat7/iBook.sql
+++ iBook-hibernate-tomcat7/iBook.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.23, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.24, for Linux (x86_64)
 --
 -- Host: localhost    Database: iBook
 -- ------------------------------------------------------
--- Server version	5.5.23
+-- Server version	5.5.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +18,9 @@
 --
 -- Table structure for table `authors`
 --
+CREATE database IF NOT EXISTS iBook;
+
+USE DATABASE iBook;
 
 DROP TABLE IF EXISTS `authors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -26,7 +29,7 @@ CREATE TABLE `authors` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `author_name` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -35,8 +38,33 @@ CREATE TABLE `authors` (
 
 LOCK TABLES `authors` WRITE;
 /*!40000 ALTER TABLE `authors` DISABLE KEYS */;
-INSERT INTO `authors` VALUES (1,'Sinnes A. Cort'),(2,'Gourmet Magazine'),(3,'Michael Symon');
+INSERT INTO `authors` VALUES (1,'Sinnes A. Cort'),(2,'Gourmet Magazine'),(3,'Michael Symon'),(4,'Thosmas H. Cormen'),(5,'Charles E. Leiserson'),(6,'Ronald L. Rivest'),(7,'Rnald L. Rivest'),(8,'Clifford Stein'),(9,'Joschua Bloch'),(10,'Tom White');
 /*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authors2Books`
+--
+
+DROP TABLE IF EXISTS `authors2Books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors2Books` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `author_id` int(10) NOT NULL,
+  `book_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authors2Books`
+--
+
+LOCK TABLES `authors2Books` WRITE;
+/*!40000 ALTER TABLE `authors2Books` DISABLE KEYS */;
+INSERT INTO `authors2Books` VALUES (1,1,1),(2,2,2),(3,3,3),(4,4,4),(5,5,4),(6,6,4),(7,7,4),(8,8,4),(9,9,5),(10,10,6);
+/*!40000 ALTER TABLE `authors2Books` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -50,15 +78,14 @@ CREATE TABLE `books` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `category_id` int(10) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `author_id` int(10) NOT NULL,
   `isBestSeller` tinyint(1) DEFAULT NULL,
-  `price` int(4) DEFAULT NULL,
+  `price` decimal(5,2) DEFAULT NULL,
   `photo_url` varchar(255) DEFAULT NULL,
   `publish_date` int(4) NOT NULL,
   `description` varchar(255) DEFAULT NULL,
   `pages` int(5) NOT NULL DEFAULT '498',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -67,7 +94,7 @@ CREATE TABLE `books` (
 
 LOCK TABLES `books` WRITE;
 /*!40000 ALTER TABLE `books` DISABLE KEYS */;
-INSERT INTO `books` VALUES (1,4,'25 Essentials',1,1,8,'book1.jpg',2012,'Gas grills are easy, convenient, and clean. But the challenge, for new owners and even for seasoned vets, is to infuse the robust, soulful flavor of charcoal-grilled food, or, ideally, of smoke-cooked barbecue, into meals cooked on a gas grill. In 25 Esse',498),(2,4,'Gourmet Weekday: All-Time Favorite Recipes',2,0,9,'book2.jpg',2012,'Sometimes the easiest way to cook something is also the best. From the hundreds of effortless dishes published in Gourmet over the years, the editors selected the best loved, the most memorably simple, and the riotously tasty—the ones that have entered th',498),(3,4,'Michael Symon\'s Carnivore: 120 Recipes for Meat Lovers',3,0,20,'book3.jpg',2012,'',498);
+INSERT INTO `books` VALUES (1,4,'25 Essentials',1,8.00,'book1.jpg',2012,'Gas grills are easy, convenient, and clean. But the challenge, for new owners and even for seasoned vets, is to infuse the robust, soulful flavor of charcoal-grilled food, or, ideally, of smoke-cooked barbecue, into meals cooked on a gas grill. In 25 Esse',498),(2,4,'Gourmet Weekday: All-Time Favorite Recipes',0,9.00,'book2.jpg',2012,'Sometimes the easiest way to cook something is also the best. From the hundreds of effortless dishes published in Gourmet over the years, the editors selected the best loved, the most memorably simple, and the riotously tasty—the ones that have entered th',498),(3,4,'Michael Symon\'s Carnivore: 120 Recipes for Meat Lovers',0,20.00,'book3.jpg',2012,'',498),(4,5,'Introduction to Algorithms',1,69.35,'book4.jpg',2009,'Some books on algorithms are rigorous but incomplete; others cover masses of material but lack rigor. Introduction to Algorithms uniquely combines rigor and comprehensiveness. The book covers a broad range of algorithms in depth, yet makes their design an',350),(5,5,'Effective Java (2nd Edition)',1,41.11,'book5.jpg',2008,'Are you looking for a deeper understanding of the Java™ programming language so that you can write code that is clearer, more correct, more robust, and more reusable? Look no further! Effective Java™, Second Edition, brings together seventy-eight indispen',350),(6,5,'Hadoop: The Definitive Guide',0,31.17,'book6.jpg',2012,'Ready to unlock the power of your data? With this comprehensive guide, you’ll learn how to build and maintain reliable, scalable, distributed systems with Apache Hadoop. This book is ideal for programmers looking to analyze datasets of any size, and for a',270);
 /*!40000 ALTER TABLE `books` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -96,6 +123,39 @@ INSERT INTO `categories` VALUES (1,'Literature & Fiction'),(2,'Romance'),(3,'Sci
 UNLOCK TABLES;
 
 --
+-- Table structure for table `userPayments2books`
+--
+
+DROP TABLE IF EXISTS `userPayments2books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userPayments2books` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `transaction_id` int(10) DEFAULT NULL,
+  `book_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transactionId` (`transaction_id`),
+  CONSTRAINT `userPayments2books_ibfk_1` FOREIGN KEY (`transaction_id`) REFERENCES `user_payments` (`transaction_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_payments`
+--
+
+DROP TABLE IF EXISTS `user_payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_payments` (
+  `transaction_id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `paid` decimal(5,2) DEFAULT NULL,
+  `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`transaction_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `users`
 --
 
@@ -109,17 +169,31 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `create_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Table structure for table `wishbooks`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'anna1986','anna@anna.com','Y0dGemMzZHZjbVE9','2012-05-24 15:09:32'),(2,'testuser','anna@anna.com','Y0dGemMzZHZjbVE9','2012-05-25 07:52:29'),(3,'test','anna@anna.com','Y0dGemMzZHZjbVE9','2012-05-25 08:18:43'),(4,'test1','anna@anna.com','Y0dGemMzZHZjbVE9','2012-05-25 08:23:00'),(5,'hi','anna@anna.com','cGFz','2012-05-25 08:27:43'),(6,'test2','anna@anna.com','cGFzc3dvcmQ=','2012-05-25 08:50:31');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+DROP TABLE IF EXISTS `wishbooks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wishbooks` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) NOT NULL,
+  `book_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wishbooks`
+--
+
+LOCK TABLES `wishbooks` WRITE;
+/*!40000 ALTER TABLE `wishbooks` DISABLE KEYS */;
+/*!40000 ALTER TABLE `wishbooks` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -131,4 +205,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-05-25 17:42:17
+-- Dump completed on 2012-07-04 12:08:34
